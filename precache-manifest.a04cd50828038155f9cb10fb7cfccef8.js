685bde86e459047ba43246976c074086900d7cad
liquidslr <gaurav.kumar9825@gmail.com>
2020-04-26 21:15:05 +0530

    Updates

---
+++ precache-manifest.a04cd50828038155f9cb10fb7cfccef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f288d7e93dc1305567dde554f4293ce",
+    "url": "/vernacular-task/index.html"
+  },
+  {
+    "revision": "6010dd9a586769740d7f",
+    "url": "/vernacular-task/static/css/main.34fb0a0b.chunk.css"
+  },
+  {
+    "revision": "eb75df9a6be2a56532a4",
+    "url": "/vernacular-task/static/js/2.46f1e8ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vernacular-task/static/js/2.46f1e8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6010dd9a586769740d7f",
+    "url": "/vernacular-task/static/js/main.78d29ba6.chunk.js"
+  },
+  {
+    "revision": "e67e48c8d11a3abddbb4",
+    "url": "/vernacular-task/static/js/runtime-main.42d79e99.js"
+  }
+]);
